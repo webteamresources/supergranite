703db61047be5a51e9fad86cf2a2dc39b756c13c
ramchat007@gmail.com <rupesh.mestry@coxandkings.com>
2019-12-09 13:28:11 +0530

    second updates

--- src/assets/shared/product.model.ts
+++ src/assets/shared/product.model.ts
@@ -3,17 +3,9 @@ export class ProductModel {
     public category:String;
     public imagePath:String;
 
-<<<<<<< HEAD
-    constructor(name: string, category:string, desc: string, imagePath: string) {
-        this.name = name;
-        this.description = desc;
-        this.imagePath = imagePath;
-        this.category = category;
-=======
     constructor(name: string, category:string, imagePath:string){
         this.name = name;
         this.category = category;
         this.imagePath = imagePath;
->>>>>>> a40e41fc068eb3908cef4b1b136b9aeed44ae78b
     }
 }
