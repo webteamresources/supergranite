61e3c9bedefa8abab0871839756ae41fbf75346a
webteamresources <narendra.bait@CNKGLOBE.COM>
2019-12-10 11:18:17 +0530

    getting updated on about

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ngModel } from '@angular/forms'
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  fname = '';
 
   constructor() { }
 
