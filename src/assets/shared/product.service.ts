ed399651c3995141971b8f112fa317674d043da1
ramchat007@gmail.com <rupesh.mestry@coxandkings.com>
2019-12-10 11:14:27 +0530

    latest updates

--- src/assets/shared/product.service.ts
+++ src/assets/shared/product.service.ts
@@ -1,40 +1,5 @@
 import { ProductModel } from './product.model';
 
 export class ProductService {
-    private products1: ProductModel[] = [
-        new ProductModel('test 1', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg'),
-        new ProductModel('test 1', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg'),
-        new ProductModel('test 1', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg'),
-        new ProductModel('test 1', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg')
-    ]
-    getProductsCat1() {
-        return this.products1.slice();
-    }
-    private products2: ProductModel[] = [
-        new ProductModel('test 2', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg'),
-        new ProductModel('test 2', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg'),
-        new ProductModel('test 2', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg'),
-        new ProductModel('test 2', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg')
-    ]
-    getProductsCat2() {
-        return this.products2.slice();
-    }
-    private products3: ProductModel[] = [
-        new ProductModel('test 3', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg'),
-        new ProductModel('test 3', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg'),
-        new ProductModel('test 3', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg'),
-        new ProductModel('test 3', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg')
-    ]
-    getProductsCat3() {
-        return this.products3.slice();
-    }
-    private products4: ProductModel[] = [
-        new ProductModel('test 4', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg'),
-        new ProductModel('test 4', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg'),
-        new ProductModel('test 4', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg'),
-        new ProductModel('test 4', 'test cate', '../assets/resources/images/products/granite-products/granite-blocks.jpg')
-    ]
-    getProductsCat4() {
-        return this.products4.slice();
-    }
+    private products1: ProductModel[] = [];
 }
