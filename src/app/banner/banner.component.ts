bd7c04abb7e94c6651351e592adde0a9d72599f1
ramchat007@gmail.com <rupesh.mestry@coxandkings.com>
2019-12-12 19:34:26 +0530

    static new updates\

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { BannerService } from './../../assets/shared/banner.service';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Result } from './results';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  encapsulation: ViewEncapsulation.Native
 })
 export class BannerComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  sliderArray: object[];
+  transform: number;
+  selectedIndex = 0;
+  
+  constructor(private bannerService: BannerService) { 
+    this.sliderArray = [];    
+    this.selectedIndex = 0;
+    this.transform = 100;
   }
+  
+  ngOnInit() {
+    this.bannerService.getData().subscribe((result: Result)=>{
+      this.sliderArray = result.sliderArray;
+    })
+  }
+
+  selected(x) {
+    this.downSelected(x);
+    this.selectedIndex = x;
+   }
+
+   keySelected(x) {
+    this.downSelected(x);
+    this.selectedIndex = x;
+  }
+
+   downSelected(i) {
+   this.transform =  100 - (i) * 50;
+     this.selectedIndex = this.selectedIndex + 1;
+     if (this.selectedIndex > this.sliderArray.length) {
+       this.selectedIndex = 0;
+     }
+   }
+
 
 }
---
+++ src/app/banner/results.ts
@@ -0,0 +1,6 @@
+export interface Result {
+    sliderArray: [
+      {'img': string, 'alt': string, 'text': string }
+    ];
+  }
+  
