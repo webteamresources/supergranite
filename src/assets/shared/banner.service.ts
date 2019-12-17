1cbaacbb11ba78e6197a66461e9cd0f4885ef401
ramchat007@gmail.com <rupesh.mestry@coxandkings.com>
2019-12-17 16:21:51 +0530

    other pages

--- src/assets/shared/banner.service.ts
+++ src/assets/shared/banner.service.ts
@@ -1,10 +1,3 @@
-import { Injectable } from '@angular/core';
-
-const URL = '../../assets/content/banner-data.json';
-
-@Injectable({
- providedIn: 'root'
-})
 export class BannerService {
 
  constructor() { }
