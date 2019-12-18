7d4b49fb40b083c00daa9d4fd703e1cec8a67cb6
webteamresources <narendra.bait@CNKGLOBE.COM>
2019-12-18 14:35:27 +0530

    updates revelent to home cara & Css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule, OwlOptions  } from 'ngx-owl-carousel-o';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { ProductService } from './../../assets/shared/product.service';
 import { ProductModel } from './../../assets/shared/product.model';
@@ -8,7 +10,76 @@ import { ProductModel } from './../../assets/shared/product.model';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
+@NgModule({
+  imports: [ CarouselModule ],
+  declarations: [ HomeComponent ]
+})
 export class HomeComponent implements OnInit {
+  customOptions01: OwlOptions = {
+    center: false,
+    loop: false,
+    items:1,
+    margin: 30,
+    stagePadding: 0,
+    nav: false,
+    navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+    responsive:{
+      0:{
+        items: 1
+      },
+      480:{
+        items: 2
+      },
+      992:{
+        items: 3
+      }
+    }
+  }
+
+  customOptions02: OwlOptions = {
+    center: false,
+    loop: false,
+    items:1,
+    margin: 30,
+    stagePadding: 0,
+    nav: false,
+    navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+    responsive:{
+      0:{
+        items: 1
+      },
+      480:{
+        items: 2
+      },
+      992:{
+        items: 3
+      }
+    }
+  }
+
+  customOptions03: OwlOptions = {
+    center: false,
+    loop: false,
+    items:1,
+    margin: 30,
+    stagePadding: 0,
+    nav: false,
+    navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
+    responsive:{
+      0:{
+        items: 1
+      },
+      480:{
+        items: 2
+      },
+      992:{
+        items: 3
+      }
+    }
+  }
+
   constructor(private httpService: HttpClient, private productService: ProductService) { }
 
   configUrl = '../../../assets/content/products.json';
