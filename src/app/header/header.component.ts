56e1bcd2f62e471961f125d2cd9d680150b69eec
ramchat007@gmail.com <rupesh.mestry@coxandkings.com>
2019-12-18 19:00:14 +0530

    product updates

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
@@ -7,21 +6,10 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  configUrl = "../../assets/content/common.json";
-  commonData;
+ constructor() { }
 
-  constructor(private http: HttpClient) { }
-
-  ngOnInit() {    
-    this.http.get(this.configUrl).subscribe(
-      data => {
-        this.commonData = data as string [];
-      },
-      (err: HttpErrorResponse) => {
-        console.log (err.message);
-      }
-    );
-  };
+  ngOnInit() {
+  }
 
 
 }
